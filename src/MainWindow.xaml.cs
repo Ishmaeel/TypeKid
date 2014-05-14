d27d0de7efa27fc01cd1b4806f81a568bb07b8a6
Ishmaeel <ishmaeel@gmail.com>
2014-05-14 23:27:53 +0300

    Added some more fun symbols for "dead keys". Added special processing for Alt key. Added "clear screen" shortcuts, ESC and F5.

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -25,6 +25,16 @@ namespace TypeKid
 
         private void MainText_PreviewKeyDown(object sender, KeyEventArgs e)
         {
+            TextBox textBox = (TextBox)sender;
+
+            switch (e.Key)
+            {
+                case Key.Escape:
+                case Key.F5:
+                    textBox.Text = string.Empty;
+                    return;
+            }
+
             string replacement = null;
 
             switch (e.Key)
@@ -41,15 +51,30 @@ namespace TypeKid
                 case Key.Clear: replacement = "\u263A"; break;
                 case Key.Insert: replacement = "\u25A0"; break;
                 case Key.Delete: replacement = "\u25A1"; break;
+                case Key.Apps: replacement = "\u2261"; break;
+                case Key.Tab: replacement = "\u25AC"; break;
+                case Key.Capital: replacement = "\u2206"; break;
+                case Key.LeftShift:
+                case Key.RightShift: replacement = "\u2248"; break;
+                case Key.LeftCtrl:
+                case Key.RightCtrl: replacement = "\u2022"; break;
+                case Key.System: replacement = "\u266A"; break;
             }
 
             if (replacement != null)
             {
                 e.Handled = true;
-                Int32 lastLocation = ((TextBox)sender).SelectionStart;
-                ((TextBox)sender).Text = ((TextBox)sender).Text.Insert(lastLocation, replacement);
-                ((TextBox)sender).SelectionStart = lastLocation + 1;
+                textBox.AppendTextEx(replacement);
             }
+
+            if (e.Key == Key.System)
+                e.Handled = false;
+        }
+
+        private void MainText_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.System)
+                e.Handled = true;
         }
     }
 }
---
+++ src/TextBoxExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace TypeKid
+{
+    static class TextBoxExtensions
+    {
+        /// <summary>
+        /// Appends a string to the contents of a text control. The original method has a problem
+        /// where sometimes the caret position is not updated. This one does not.
+        /// </summary>
+        /// <param name="textBox">The textBox?</param>
+        /// <param name="textData">A string that specifies the text to append to the current contents of the text control.</param>
+        public static void AppendTextEx(this TextBox textBox, string textData)
+        {
+            Int32 lastLocation = textBox.SelectionStart;
+            textBox.Text = textBox.Text.Insert(lastLocation, textData);
+            textBox.SelectionStart = lastLocation + 1;
+        }
+    }
+}
